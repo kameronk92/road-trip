b82ec5a2efa510300249ab26a715629f948ad338
Kam Kennedy <kameronk013@gmail.com>
2024-01-16 09:38:23 -0700

    sessions happy path complete

--- spec/requests/api/v0/sessions_requests/new_session_spec.rb
+++ spec/requests/api/v0/sessions_requests/new_session_spec.rb
@@ -3,30 +3,37 @@ require 'rails_helper'
 RSpec.describe 'POST /api/v0/sessions', type: :request do
   describe 'happy path' do
     it 'can create a new session' do
-      user_params = {
+      new_user_params = {
         email: "whatever@example.com",
-        "password": "password"
+        password: "password",
+        password_confirmation: "password"
+      }
+      session_user_params = {
+        email: "whatever@example.com",
+        password: "password",
       }
 
-      post '/api/v0/sessions', params: user_params
+      Users.create!(new_user_params)
+
+      post '/api/v0/sessions', params: session_user_params, as: :json
 
       expect(response).to be_successful
       expect(response.status).to eq(200)
-      response = JSON.parse(response.body, symbolize_names: true)
+      body = JSON.parse(response.body, symbolize_names: true)
 
-      expect(response).to be_a(Hash)
-      expect(response).to have_key(:data)
-      expect(response[:data]).to be_a(Hash)
-      expect(response[:data]).to have_key(:id)
-      expect(response[:data][:id]).to be_a(String)
-      expect(response[:data]).to have_key(:type)
-      expect(response[:data][:type]).to be("users")
-      expect(response[:data]).to have_key(:attributes)
-      expect(response[:data][:attributes]).to be_a(Hash)
-      expect(response[:data][:attributes]).to have_key(:email)
-      expect(response[:data][:attributes][:email]).to be_a(String)
-      expect(response[:data][:attributes]).to have_key(:api_key)
-      expect(response[:data][:attributes][:api_key]).to be_a(String)
+      expect(body).to be_a(Hash)
+      expect(body).to have_key(:data)
+      expect(body[:data]).to be_a(Hash)
+      expect(body[:data]).to have_key(:id)
+      expect(body[:data][:id]).to be_a(String)
+      expect(body[:data]).to have_key(:type)
+      expect(body[:data][:type]).to be("users")
+      expect(body[:data]).to have_key(:attributes)
+      expect(body[:data][:attributes]).to be_a(Hash)
+      expect(body[:data][:attributes]).to have_key(:email)
+      expect(body[:data][:attributes][:email]).to be_a(String)
+      expect(body[:data][:attributes]).to have_key(:api_key)
+      expect(body[:data][:attributes][:api_key]).to be_a(String)
     end
   end
 end
