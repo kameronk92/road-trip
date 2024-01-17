38f15fc9800a48e9e459f7e127fe3fb522a53f77
Kam Kennedy <kameronk013@gmail.com>
2024-01-16 18:01:44 -0700

    forecast returning for destination

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,12 +1,13 @@
 class RoadTripFacade
   def self.get_road_trip(origin, destination)
-    data = {}
     # get travel time, convert to unix for forecast
     time_hash = MapquestService.get_travel_time(origin, destination)
-    
     travel_time = time_hash[:travel_time]
-
     unix_travel_time = time_hash[:unix]
+    
+    #format time for forecast request
+    arrival_time = Time.at(Time.now.to_i + unix_travel_time)
+    formatted_time = arrival_time.strftime("%Y-%m-%d")
 
     # get arrival coordinates
     coordinates = MapquestService.get_coordinates(destination)
@@ -15,10 +16,11 @@ class RoadTripFacade
 
     data = {
       start_city: origin,
-      destination: destination,
+      end_city: destination,
       travel_time: travel_time,
       unix_travel_time: unix_travel_time,
-      forecast: forecast
+      forecast: forecast,
+      arrival_time: arrival_time
     }
 
     RoadTrip.new(data)
--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -3,15 +3,27 @@ class RoadTrip
 
   def initialize(data)
     @id = nil
-    @start_city = data[:origin]
-    @end_city = data[:destination]
+    @start_city = data[:start_city]
+    @end_city = data[:end_city]
     @travel_time = data[:travel_time]
-    @weather_at_eta = arrival_forecast(forecast)
+    @weather_at_eta = arrival_forecast(data)
   end
 
-  def arrival_forecast(forecast)
-    forecast_hour = travel
-    require 'pry'; binding.pry
+  def arrival_forecast(data)
+    
+    time_string = data[:arrival_time]
+    
+    # Extract the arrival hour as an integer
+    hour = time_string.hour
+    
+    # plug hour into forecast
+    big_forecast = data[:forecast][:forecast][:forecastday].first[:hour][hour]
+    
+    {
+      temperature: big_forecast[:temp_f],
+      datetime: big_forecast[:time],
+      condition: big_forecast[:condition][:text]
+    }
   end
 
   # arrival_time = Time.at(Time.now.to_i + unix_travel_time)
