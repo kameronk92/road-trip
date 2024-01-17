c4e518ca5c8235048b3e8bc16ae90015e6915366
Kam Kennedy <kameronk013@gmail.com>
2024-01-16 17:38:35 -0700

    refactor road trip poro

--- app/facades/road_trip_facade.rb
+++ app/facades/road_trip_facade.rb
@@ -1,17 +1,26 @@
 class RoadTripFacade
   def self.get_road_trip(origin, destination)
+    data = {}
     # get travel time, convert to unix for forecast
     time_hash = MapquestService.get_travel_time(origin, destination)
-    travel_time = time_hash[:travel_time]
-    unix_travel_time = time_hash[:unix]
     
-    arrival_time = Time.now.to_i + unix_travel_time
+    travel_time = time_hash[:travel_time]
+
+    unix_travel_time = time_hash[:unix]
 
     # get arrival coordinates
     coordinates = MapquestService.get_coordinates(destination)
 
-    forecast = WeatherApiService.get_forecast(coordinates, arrival_time)
+    forecast = WeatherApiService.get_forecast(coordinates, formatted_time)
 
-    RoadTrip.new(origin, destination, travel_time, forecast)
+    data = {
+      start_city: origin,
+      destination: destination,
+      travel_time: travel_time,
+      unix_travel_time: unix_travel_time,
+      forecast: forecast
+    }
+
+    RoadTrip.new(data)
   end
 end
--- app/poros/road_trip.rb
+++ app/poros/road_trip.rb
@@ -1,11 +1,18 @@
 class RoadTrip
   attr_reader :id, :start_city, :end_city, :travel_time, :weather_at_eta
 
-  def initialize(origin, destination, travel_time, forecast)
+  def initialize(data)
     @id = nil
-    @start_city = origin
-    @end_city = destination
-    @travel_time = travel_time
-    @weather_at_eta = forecast
+    @start_city = data[:origin]
+    @end_city = data[:destination]
+    @travel_time = data[:travel_time]
+    @weather_at_eta = arrival_forecast(forecast)
   end
+
+  def arrival_forecast(forecast)
+    forecast_hour = travel
+    require 'pry'; binding.pry
+  end
+
+  # arrival_time = Time.at(Time.now.to_i + unix_travel_time)
 end
